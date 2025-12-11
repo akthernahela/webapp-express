6d41a870b483967e0a525d450e8ae0d132afb873
akthernahela <nahela00@gmail.com>
2025-12-11 12:53:32 +0100

    Import cors

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const PORT = 3000
+const cors = require('cors')
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`)
