3e2cdaea35e2586532cc7148e1b9cb0ec6ed1868
akthernahela <nahela00@gmail.com>
2025-12-11 12:54:45 +0100

    Add the frontend to run with

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express()
 const PORT = 3000
 const cors = require('cors')
 
+app.use(cors({ origin: 'http://localhost:5173' }));
+
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`)
 })
