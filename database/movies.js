841edae2942ad99ac38b1dca93f89b0c406cc197
akthernahela <nahela00@gmail.com>
2025-12-11 01:06:16 +0100

    Add the root show

--- database/movies.js
+++ database/movies.js
@@ -4,3 +4,9 @@ const router = express.Router()
 router.get('/', (req, res) => {
     res.send('All movies')
 })
+
+router.get('/:id', (req, res) => {
+    res.send('Show the requested movie by id' + req.params.id)
+})
+
+module.exports = router
