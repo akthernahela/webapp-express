11eef0cf25b1565b939182d121442878efd4d323
akthernahela <nahela00@gmail.com>
2025-12-11 21:26:47 +0100

    Server isn't still working

--- server.js
+++ server.js
@@ -4,12 +4,13 @@ const PORT = 3000
 const cors = require('cors')
 const notFound = require('./middlewares/notFound')
 const serverDown = require('./middlewares/serverDown')
+const moviesRouter = require('./moviesRouter')
 
 app.use(cors({ origin: 'http://localhost:5173' }));
-
+app.use(express.json());
+app.use('/api/movies', moviesRouter);
+app.use(notFound);
+app.use(serverDown);
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`)
 })
-
-app.use(serverDown);
-app.use(notFound);
