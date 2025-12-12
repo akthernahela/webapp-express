728e479105a84f61f31805001b5cc778bc26b651
akthernahela <nahela00@gmail.com>
2025-12-12 12:58:42 +0100

    Fix the server because it's not working

--- server.js
+++ server.js
@@ -1,18 +1,13 @@
 const express = require('express')
 const app = express()
 const PORT = 3000
-const cors = require('cors')
-const notFound = require('./middlewares/notFound')
-const serverDown = require('./middlewares/serverDown')
+
 
 app.get('/', (res, req) => {
     res.send('Movies Catalog')
 });
 
-app.use(cors({ origin: 'http://localhost:5173' }));
-app.use(express.json());
-app.use(notFound);
-app.use(serverDown);
+
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`)
 })
