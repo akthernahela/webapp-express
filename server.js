f7478947544c7fd4d2921df955c97ba81af35791
akthernahela <nahela00@gmail.com>
2025-12-12 13:05:01 +0100

    Register with express json

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`)
+})
+
+app.use(express.json())
 
 app.get('/', (res, req) => {
     res.send('Movies Catalog')
 });
 
 
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`)
-})
