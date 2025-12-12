0de3479062592710e657e1153467a6dfd854c61f
akthernahela <nahela00@gmail.com>
2025-12-12 12:36:42 +0100

    Fix the backend

--- server.js
+++ server.js
@@ -4,11 +4,10 @@ const PORT = 3000
 const cors = require('cors')
 const notFound = require('./middlewares/notFound')
 const serverDown = require('./middlewares/serverDown')
-const moviesRouter = require('./moviesRouter')
+
 
 app.use(cors({ origin: 'http://localhost:5173' }));
 app.use(express.json());
-app.use('/api/movies', moviesRouter);
 app.use(notFound);
 app.use(serverDown);
 app.listen(PORT, () => {
