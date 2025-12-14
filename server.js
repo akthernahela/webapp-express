593973a3a4b8b4da4722b6ac077015058bd7fc49
akthernahela <nahela00@gmail.com>
2025-12-14 20:06:04 +0100

    Change the middlewares position

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const connection = require('./database/connection')
 const moviesRouter = require('./database/movies')
 const cors = require('cors');
 
+app.use(cors({
+    origin: 'http://localhost:5173'
+}));
+
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 })
@@ -16,7 +20,3 @@ app.get('/', (req, res) => {
 })
 
 app.use('/api/movies', moviesRouter)
-
-app.use(cors({
-    origin: 'http://localhost:5173'
-}));
