0c94d82ff7c0318f038e61b2d30181c3d83db077
akthernahela <nahela00@gmail.com>
2025-12-14 18:35:00 +0100

    Register the body parser

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 })
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
     res.send('My movie Catalog')
 })
